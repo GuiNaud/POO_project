4c8bd85890717e78c05c88643b9cb25903433eb1
AT <atouzardpro@gmail.com>
2015-10-13 12:59:04 +0200

    starting town class

--- www/class/town.class.php
+++ www/class/town.class.php
@@ -1,11 +1,46 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: guillaumerannaud
- * Date: 13/10/15
- * Time: 10:25
- */
 
-class Town {
+abstract class Town
+{
+    protected $name;
+    protected $level = 0;
+    protected $turn = 0;
+    protected $event;
+    protected $zone = array();
+    protected $population;
+    protected $populationmax;
+    protected $populationactive;
+    protected $gold;
+    protected $wood;
+    protected $stone;
+    protected $food;
+    protected $prosperity;
 
+    public function __construct($gold, $wood, $stone, $food, $prosperity)
+    {
+        $this->gold = $gold;
+        $this->wood = $wood;
+        $this->stone = $stone;
+        $this->food = $food;
+        $this->prosperity = $prosperity;
+    }
+
+    protected abstract function harvest();
+
+    protected function createBuilding($position, $type)
+    {
+        $this->zone[$position] = new $type();
+    }
+
+    protected function destructBuilding($position)
+    {
+        $this->zone[$position] = null;
+    }
+
+    protected function getEvent()
+    {
+
+    }
 }
+
+?>
